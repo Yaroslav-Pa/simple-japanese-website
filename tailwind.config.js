a19aa53935960d06c58394fc068f82ebd7384df1
Yaroslav-Pa <yaroslav.roro@gmail.com>
2023-11-24 01:13:30 +0200

    adding main img

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'mainBg': "url('/src/img/main-min.jpg')"
+      }
+    },
   },
   plugins: [],
 }
